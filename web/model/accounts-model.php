5074ac0aedb779e6a4002708dfa40de0eff570bb
Tyson Thornton <tho16106@byui.edu>
2020-12-03 00:18:07 -0700

    added files

--- web/model/accounts-model.php
+++ web/model/accounts-model.php
@@ -5,7 +5,7 @@
 function getUser($userEmail)
 {
     $db = dbConnect();
-    $sql = 'SELECT username, useremail, userpassword, FROM public.user WHERE useremail = $1';
+    $sql = 'SELECT username, useremail, userpassword FROM public.user WHERE useremail = $1';
     $userData = pg_prepare($db, 'query', $sql);
     $userData = pg_execute($db, 'query', array($userEmail));
     echo $userData;
