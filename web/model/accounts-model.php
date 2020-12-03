54916c98fb641b3948c1547c9038ff1f3e706473
Tyson Thornton <tho16106@byui.edu>
2020-12-02 23:28:31 -0700

    added files

--- web/model/accounts-model.php
+++ web/model/accounts-model.php
@@ -5,13 +5,15 @@
 function getUser($userEmail)
 {
     $db = dbConnect();
-    $sql = "SELECT username, useremail, userpassword, FROM public.user WHERE useremail = '$userEmail'";
+    $sql = 'SELECT username, useremail, userpassword, FROM public.user WHERE useremail = $1';
+    $userData = pg_prepare($db, 'query', $sql);
+    $userData = pg_execute($db, 'query', array($userEmail));
 
     
-    $stmt = $db->prepare($sql);
-    $stmt->bindValue(':email', $userEmail, PDO::PARAM_STR);
-    $stmt->execute();
-    $userData = $stmt->fetch(PDO::FETCH_ASSOC);
-    $stmt->closeCursor();
+    // $stmt = $db->prepare($sql);
+    // $stmt->bindValue(':email', $userEmail, PDO::PARAM_STR);
+    // $stmt->execute();
+    // $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+    // $stmt->closeCursor();
     return $userData;
 }
