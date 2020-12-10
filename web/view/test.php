3dc1bd296c5970f64b0a864b1203da0fd5448c0b
Tyson Thornton <tho16106@byui.edu>
2020-12-09 18:51:40 -0700

    added files

---
+++ web/view/test.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="/css/small.css" type="text/css" rel="stylesheet" media="screen">
+    <link href="/css/large.css" type="text/css" rel="stylesheet" media="screen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="My Vinyl Shelf is a website to digitally store your vinyl record collection">
+    <title>Vinyl Record Playlist</title>
+</head>
+
+<body>
+    <header> <?php include $_SERVER['DOCUMENT_ROOT'] . '/pageSections/header.php'; ?> </header>
+    <nav> <?php include $_SERVER['DOCUMENT_ROOT'] . '/pageSections/nav.php'; ?></nav>
+
+    <main>
+
+    <?php
+            require('../vendor/autoload.php');
+            // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+            $s3 = new Aws\S3\S3Client([
+                'version'  => '2006-03-01',
+                'region'   => 'us-west-1',
+            ]);
+            $bucket = getenv('S3_BUCKET_NAME') ?: die('No "S3_BUCKET" config var in found in env!');
+            ?>
+
+            <h1>S3 upload example</h1>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+                // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
+                try {
+                    // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
+                    $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+            ?>
+                    <p>Upload <a href="<?= htmlspecialchars($upload->get('ObjectURL')) ?>">successful</a> :)</p>
+                <?php } catch (Exception $e) { ?>
+                    <p>Upload error :(</p>
+            <?php }
+            } ?>
+            <p>Image</p>
+            <form enctype="multipart/form-data" action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                <input name="userfile" type="file">
+                <input type="submit" name='submit' value="Save"><br>
+            </form>
+        </div>
+
+
+    </main>
+    <footer> <?php include $_SERVER['DOCUMENT_ROOT'] . '/pageSections/footer.php'; ?></footer>
+</body>
+
+</html>
+<?php
+// Unset the stored message
+unset($_SESSION['message']);
+?>
