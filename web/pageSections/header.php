6d774f3104ecc305216ea207c50e455df79c5ac8
Tyson Thornton <tho16106@byui.edu>
2020-12-13 13:05:23 -0700

    added files

--- web/pageSections/header.php
+++ web/pageSections/header.php
@@ -5,7 +5,7 @@
 <?php 
     if (isset($_SESSION['loggedin'])) {
 
-    echo "<div class='accountOptions'";
+    echo "<div class='accountOptions'>";
     echo "<a id='profile' href='../accounts/index.php?action=Profile'>Account Profile</a>";
     echo "<a id='logout' href='../accounts/index.php?action=Logout'>Logout</a>";
     echo "</div>";
