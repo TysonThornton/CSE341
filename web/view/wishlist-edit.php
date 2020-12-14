2be7e8fec92f093cbd9d558da4e3bb1b27e0a974
Tyson Thornton <tho16106@byui.edu>
2020-12-14 02:03:29 -0700

    added files

--- web/view/wishlist-edit.php
+++ web/view/wishlist-edit.php
@@ -14,47 +14,43 @@
     <header> <?php include $_SERVER['DOCUMENT_ROOT'] . '/pageSections/header.php'; ?> </header>
     <nav> <?php include $_SERVER['DOCUMENT_ROOT'] . '/pageSections/nav.php'; ?></nav>
 
-    <main>
+    <main class="editWishlist">
         <div class='formcontainer'>
 
             <div class="formHeader">
             <h1><?php if (isset($wlVinylAlbum) && isset($wlVinylBand)) {
-                    echo "Edit Wishlist Item: $wlVinylAlbum by $wlVinylBand";
+                    echo "Edit $wlVinylAlbum by $wlVinylBand";
                 }
                 ?></h1>
                 <p>Edit the wishlist item below.</p>
             </div>
 
-            <div class="dbMessage">
+            
                 <?php
                 if (isset($message)) {
                     echo $message;
                 }
                 ?>
-            </div>
+            
 
-            <form class='vinylForm' action="../wishlist/index.php" method="post">
+            <form class='form' action="../wishlist/index.php" method="post">
                 <fieldset>
-                <label for="wlVinylBand">Artist / Band </label><br>
+                <label for="wlVinylBand">Artist / Band </label>
                     <input type='text' name="wlVinylBand" id="wlVinylBand" placeholder="required" class='input' required <?php if (isset($wlVinylBand)) {
                                                                                         echo "value='$wlVinylBand'";
-                                                                                    }  ?>><br>
-                    <label for="wlVinylAlbum">Album Name</label><br>
+                                                                                    }  ?>>
+                    <label for="wlVinylAlbum">Album Name</label>
                     <input type='text' name="wlVinylAlbum" id="wlVinylAlbum" placeholder="required" class='input' required <?php if (isset($wlVinylAlbum)) {
                                                                                             echo "value='$wlVinylAlbum'";
-                                                                                        }  ?>><br>
-                    <label for="wlVinylPrice">Price</label><br>
+                                                                                        }  ?>>
+                    <label for="wlVinylPrice">Price</label>
                     <input type='number' name="wlVinylPrice" id="wlVinylPrice" placeholder="required" class='input' required <?php if (isset($wlVinylPrice)) {
                                                                                             echo "value='$wlVinylPrice'";
-                                                                                        }  ?>><br>
-                    <label for="wlVinylNotes">Notes</label><br>
+                                                                                        }  ?>>
+                    <label for="wlVinylNotes">Notes</label>
                     <textarea name="wlVinylNotes" id="wlVinylNotes" class='input'><?php if (isset($wlVinylNotes)) {
                                                                                                     echo "$wlVinylNotes";
-                                                                                                }  ?></textarea><br>
-                    <label for="wlVinylImage">Image</label><br>
-                    <input type="text" name="wlVinylImage" id="wlVinylImage" class='input' <?php if (isset($wlVinylImage)) {
-                                                                                            echo "value='$wlVinylImage'";
-                                                                                        }  ?>><br>
+                                                                                                }  ?></textarea>
 
                     <input type='submit' name='submit' value='Save Changes' class='submitVinyl'>
                     <!--Add the action key - value pair -->
