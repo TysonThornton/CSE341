23228c781a84613413eb69277a076198087e99e4
Tyson Thornton <tho16106@byui.edu>
2020-12-18 00:36:45 -0700

    updated files

--- web/view/vinyl-collection.php
+++ web/view/vinyl-collection.php
@@ -9,8 +9,8 @@ if (!isset($_SESSION['loggedin'])) {
 
 <head>
     <meta charset="UTF-8">
-    <link href="/css/small.css" type="text/css" rel="stylesheet" media="screen">
-    <link href="/css/large.css" type="text/css" rel="stylesheet" media="screen">
+    <link href="../css/small.css" type="text/css" rel="stylesheet" media="screen">
+    <link href="../css/large.css" type="text/css" rel="stylesheet" media="screen">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="My Vinyl Shelf is a website to digitally store your vinyl record collection">
     <title>Vinyl Record Playlist | Vinyl Collection</title>
